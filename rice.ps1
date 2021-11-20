a8e8589aea4590c3c918187e522c857af118802e
David <davidnickromero@gmail.com>
2021-11-19 23:00:11 -0800

    fixed variables

--- rice.ps1
+++ rice.ps1
@@ -1,7 +1,7 @@
 ## Change to your favorite Nerd Font and Oh-My-Posh theme ##
-$nerd-font = "CaskaydiaCove NF"
-$reserved-font-name = "cascadiacode-nf"
-$omp-theme = "tonybaloney"
+$nerd_font = "CaskaydiaCove NF"
+$reserved_font_name = "cascadiacode-nf"
+$omp_theme = "tonybaloney"
 
 # Install scoop (package manager)
 Set-ExecutionPolicy RemoteSigned -scope CurrentUser
@@ -9,7 +9,7 @@ iwr -useb get.scoop.sh | iex
 
 # Install Nerd Fonts
 scoop bucket add nerd-fonts
-scoop install $reserved-font-name
+scoop install $reserved_font_name
 
 # Install Oh-My-Posh
 Install-Module oh-my-posh -Scope CurrentUser
@@ -21,8 +21,8 @@ scoop install windows-terminal
 # Apply nerd font to Windows Terminal settings
 $p = "$env:LocalAppData\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json"
 $f = (Get-Content $p -raw | ConvertFrom-Json)
-$f.profiles.list[0].fontFace = $nerd-font
+$f.profiles.list[0].fontFace = $nerd_font
 $f | ConvertTo-Json -depth 69 | Set-Content $p
 
 # Apply theme for Oh-My-Posh
-Set-Content $PROFILE "Set-PoshPrompt -Theme $omp-theme"
+Set-Content $PROFILE "Set-PoshPrompt -Theme $omp_theme"
