78d78c514b5ee1885e919355548ede6e279cabc9
David <davidnickromero@gmail.com>
2021-12-14 19:13:09 -0800

    Fixed bugs and compatibility issues

--- rice.ps1
+++ rice.ps1
@@ -1,28 +1,42 @@
 ## Change to your favorite Nerd Font and Oh-My-Posh theme ##
-$nerd_font = "CaskaydiaCove NF"
 $reserved_font_name = "cascadiacode-nf"
 $omp_theme = "tonybaloney"
 
 # Install scoop (package manager)
-Set-ExecutionPolicy RemoteSigned -scope CurrentUser
-iwr -useb get.scoop.sh | iex
+Write-Output "Preparing to install Scoop..."
+if (!(Get-Command scoop -ErrorAction SilentlyContinue)) {
+    Write-Output "Installing Scoop..."
+    Set-ExecutionPolicy RemoteSigned -scope CurrentUser
+    Invoke-WebRequest -useb get.scoop.sh | Invoke-Expression
+} else {
+    Write-Output "Scoop already installed!"
+}
 
 # Install Nerd Fonts
+Write-Output "Preparing to install Nerd Fonts..."
+scoop install sudo
 scoop bucket add nerd-fonts
-scoop install $reserved_font_name
+sudo scoop install $reserved_font_name
+sudo scoop update $reserved_font_name
 
 # Install Oh-My-Posh
+Write-Output "Preparing to install Oh-My-Posh..."
 Install-Module oh-my-posh -Scope CurrentUser
+Update-Module oh-my-posh
+scoop install https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/oh-my-posh.json
+scoop update oh-my-posh
 
 # Install Windows Terminal
-scoop bucket add extras
-scoop install windows-terminal
-
-# Apply nerd font to Windows Terminal settings
-$p = "$env:LocalAppData\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json"
-$f = (Get-Content $p -raw | ConvertFrom-Json)
-$f.profiles.list[0].fontFace = $nerd_font
-$f | ConvertTo-Json -depth 69 | Set-Content $p
+Write-Output "Preparing to install Windows Terminal..."
+$OS = (Get-WmiObject -class Win32_OperatingSystem).Caption
+if (($OS | Select-String "Windows 10" -Quiet) -or ($OS | Select-String "Windows 11" -Quiet)) {
+    Write-Output "Installing Windows Terminal..."
+    scoop bucket add extras
+    scoop install windows-terminal
+    scoop update windows-terminal
+} else {
+    Write-Output "Couldn't install: Windows Terminal only works on Windows 10 & 11"
+}
 
 # Apply theme for Oh-My-Posh
 Set-Content $PROFILE "Set-PoshPrompt -Theme $omp_theme"
